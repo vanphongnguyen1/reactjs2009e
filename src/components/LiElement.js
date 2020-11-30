397a75665204d447b57d1978dfebe8e366f8a085
vanphongnguyen1 <61622503+vanphongnguyen1@users.noreply.github.com>
2020-11-30 18:39:33 +0700

    Debug 2nd

--- src/components/LiElement.js
+++ src/components/LiElement.js
@@ -4,27 +4,17 @@ import Link from './Link'
 class LiElement extends React.Component {
   render() {
     const elementLis = this.props.data.map((ele, index) =>  {
-      if (ele.isActive) {
-        return  <li className="menu__item active" key={ index }>
-                  <Link data={ ele } />
-                  { ele.children &&
-                    <ul className="menu__children">
-                    <LiElement data={ ele.children } />
-                    </ul>
-                  }
-                </li>
-      } else {
-        return  <li className="menu__item" key={ index }>
-                  <Link data={ ele } />
-                  { ele.children &&
-                    <ul className="menu__children">
-                    <LiElement data={ ele.children } />
-                    </ul>
-                  }
-                </li>
-      }
+        return (
+          <li className={ ele.isActive ? "menu__item active" : "menu__item" }  key={ index }>
+            <Link data={ ele } />
+            { ele.children &&
+              <ul className="menu__children">
+              <LiElement data={ ele.children } />
+              </ul>
+            }
+          </li>
+        )
     })
-
     return(
       <>
         { elementLis }
